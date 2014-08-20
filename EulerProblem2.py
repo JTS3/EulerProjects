7d4d09c123d9ebe40484fea751d70315c9d7cd78
JTS3 <JamesTSmithIII@gmail.com>
2014-08-20 17:09:30 -0400

    EulerProblem2 varnames
    Cleaned up the variable names in the final while loop.

--- EulerProblem2.py
+++ EulerProblem2.py
@@ -5,19 +5,18 @@ TopNumber = input("This program gives you the sum of the even elements of a sub-
 
 FibList = [1, 2]
 n = 0
-
 while FibList[n+1] < TopNumber:
     NextNumber = FibList[n+1] + FibList[n] 
     if ( NextNumber > TopNumber) : break
     FibList.append(NextNumber)
-    n += 1
-    
+    n += 1    
 print "The Fibbonacci sub-sequence terminating in the highest number less than", TopNumber,"is", FibList, "."
 
+
+TempEvenFibSum = 0
 x = 1
-a = 0
 while x < len(FibList):
-    EvenSumTemp = a + FibList[x] 
+    EvenFibSum = TempEvenFibSum + FibList[x] 
     x += 2
-    a = EvenSumTemp
-print "The sum of the even numbered elements of the subsequence is ", EvenSumTemp, "."
+    TempEvenFibSum = EvenFibSum
+print "The sum of the even numbered elements of the subsequence is ", EvenFibSum, "."
