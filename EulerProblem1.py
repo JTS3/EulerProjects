cd44c7260ed20b532161e1aaa43e55728e6cf372
JTS3 <JamesTSmithIII@gmail.com>
2014-08-20 16:19:54 -0400

    EulerProblem1 variable names
    Edited the EulerProblem1 variable name n to the more descriptive
    CandidateMultiple.

--- EulerProblem1.py
+++ EulerProblem1.py
@@ -2,13 +2,13 @@
 
 TopNumber = input("What is the top number?")
 MultipleList = []
-n = 1
+CandidateMultiple = 1
 
-while n < TopNumber:
-    if n % (5 * 3) == 0 : MultipleList.append(n)
-    elif n % 5 == 0 : MultipleList.append(n)
-    elif n % 3 == 0 : MultipleList.append(n)
-    n += 1
+while CandidateMultiple < TopNumber:
+    if CandidateMultiple % (5 * 3) == 0 : MultipleList.append(CandidateMultiple)
+    elif CandidateMultiple % 5 == 0 : MultipleList.append(CandidateMultiple)
+    elif CandidateMultiple % 3 == 0 : MultipleList.append(CandidateMultiple)
+    CandidateMultiple += 1
 
 print "The multiples of 5 and 3 up to", TopNumber, "are:", MultipleList, "."
 print "The sum of these multiples is", sum(MultipleList), "."
