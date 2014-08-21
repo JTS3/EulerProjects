f89cf968318890cf78d5b54904a447a168c4a720
JTS3 <JamesTSmithIII@gmail.com>
2014-08-21 13:21:31 -0400

    EulerProblem2 varnames
    Changed the variable names to reflect Python naming conventions.

--- EulerProblem2.py
+++ EulerProblem2.py
@@ -1,22 +1,21 @@
 # The following responds to Problem 2 on Project Euler (https://projecteuler.net/problem=2)
 # The problem specifies 4,000,000 as the top number, but this program allows any input number. Be careful with it.
 
-TopNumber = input("This program gives you the sum of the even elements of a sub-sequence -- up to a given top number -- of the Fibonacci sequence.  What is the top number in the Fibonacci sub-sequence?")
+top_number = input("This program gives you the sum of the even elements of a sub-sequence -- up to a given top number -- of the Fibonacci sequence.  What is the top number in the Fibonacci sub-sequence?")
 
-FibList = [1, 2]
-IndexA = 0
-while FibList[IndexA+1] < TopNumber:
-    NextNumber = FibList[IndexA+1] + FibList[IndexA] 
-    if ( NextNumber > TopNumber) : break
-    FibList.append(NextNumber)
-    IndexA += 1    
-print "The Fibbonacci sub-sequence terminating in the highest number less than", TopNumber,"is", FibList, "."
+fib_list = [1, 2]
+indexA = 0
+while fib_list[indexA+1] < top_number:
+    next_number = fib_list[indexA+1] + fib_list[indexA] 
+    if ( next_number > top_number) : break
+    fib_list.append(next_number)
+    indexA += 1    
+print "The Fibbonacci sub-sequence terminating in the highest number less than", top_number,"is", fib_list, "."
 
 
-EvenFibSum = 0
-IndexB = 1
-while IndexB < len(FibList):
-    EvenFibSum += FibList[IndexB] 
-    IndexB += 2
-    TempEvenFibSum = EvenFibSum
-print "The sum of the even numbered elements of the subsequence is ", EvenFibSum, "."
+even_fib_sum = 0
+indexB = 1
+while indexB < len(fib_list):
+    even_fib_sum += fib_list[indexB] 
+    indexB += 2
+print "The sum of the even numbered elements of the subsequence is ", even_fib_sum, "."
