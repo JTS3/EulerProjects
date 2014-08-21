d3e2574acc68fb1e76601ac94c495b8d59672d92
JTS3 <JamesTSmithIII@gmail.com>
2014-08-21 18:30:39 -0400

    EulerProblem5 should work
    This ought to work, but the run time is very long.

--- EulerProblem5.py
+++ EulerProblem5.py
@@ -1,15 +1,23 @@
-candidate_number = 2520
-bottom_number = 1
-top_number = 10
-n = bottom_number
+def MultipleCheck(local_n,local_top_number,local_candidate_number):
+    while local_n <= local_top_number:
+        if local_candidate_number % local_n <> 0:
+#            print "NO!", local_candidate_number, "divided by", local_n, "has a remainder!"
+            break
+        else:
+            print local_candidate_number, local_n  #comment this out later
+            local_n += 1  
+        if local_n > local_top_number:
+            print "success", local_candidate_number 
+            global ENDER
+            ENDER = 1 
 
 
-while n <= top_number:
-    if candidate_number % n <> 0:
-        print "NO!", candidate_number, "divided by", n, "has a remainder!"
-        break
-    else:
-        print n  #comment this out later
-        n += 1   
-if n > top_number:
-     print "success" 
+
+candidate_number = 2520  # since we know that 2520 is that smallest number divisible by all numbers from 1 to 10, we know that the number must exceed 2520
+bottom_number = 2   # this does not have to start at 1. It is obvious that N%1 = 0 for all N. 
+top_number = 20
+ENDER = 0
+
+while ENDER == 0:
+    MultipleCheck(bottom_number, top_number, candidate_number)
+    candidate_number += 1
