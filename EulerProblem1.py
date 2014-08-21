0e35ae1c513fc49e2776090692d70f25d4ae8bd2
JTS3 <JamesTSmithIII@gmail.com>
2014-08-21 13:48:59 -0400

    EulerProblem1 varnames
    Changed the variable names to reflect Python naming conventions.

--- EulerProblem1.py
+++ EulerProblem1.py
@@ -1,14 +1,14 @@
 # The following responds to Problem 1 on Project Euler (https://projecteuler.net/problem=1)
 
-TopNumber = input("This program gives you a list of multiples of 3 and 5 up to a given top number.  What is the top number?")
-MultipleList = []
-CandidateMultiple = 1
+top_number = input("This program gives you a list of multiples of 3 and 5 up to a given top number.  What is the top number?")
+multiple_list = []
+candidate_multiple = 1
 
-while CandidateMultiple < TopNumber:
-    if CandidateMultiple % (5 * 3) == 0 : MultipleList.append(CandidateMultiple)
-    elif CandidateMultiple % 5 == 0 : MultipleList.append(CandidateMultiple)
-    elif CandidateMultiple % 3 == 0 : MultipleList.append(CandidateMultiple)
-    CandidateMultiple += 1
+while candidate_multiple < top_number:
+    if candidate_multiple % (5 * 3) == 0 : multiple_list.append(candidate_multiple)
+    elif candidate_multiple % 5 == 0 : multiple_list.append(candidate_multiple)
+    elif candidate_multiple % 3 == 0 : multiple_list.append(candidate_multiple)
+    candidate_multiple += 1
 
-print "The multiples of 5 and 3 up to", TopNumber, "are:", MultipleList, "."
-print "The sum of these multiples is", sum(MultipleList), "."
+print "The multiples of 5 and 3 up to", top_number, "are:", multiple_list, "."
+print "The sum of these multiples is", sum(multiple_list), "."
