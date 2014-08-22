0e3815c8207b289068e0341bf879656884c789eb
JTS3 <JamesTSmithIII@gmail.com>
2014-08-21 20:13:25 -0400

    EulerProblem5 cleanup
    cleaning up the file

--- EulerProblem5.py
+++ EulerProblem5.py
@@ -13,15 +13,11 @@ def MultipleCheck(local_n,local_top_number,local_candidate_number):
 
 # Since we know that 2520 is that smallest number divisible by all numbers from 1 to 10, we know that the candidate number must exceed 2520
 
-candidate_number = 2520  
+candidate_number = 2520 
 bottom_number = 2   # this does not have to start at 1. It is obvious that N%1 = 0 for all N. 
-top_number = 14
+top_number = 20
 ENDER = 0
 
 while ENDER == 0:
     MultipleCheck(bottom_number, top_number, candidate_number)
     candidate_number += 1
-    
-
-# Answer found after a long run.
-# success 88648560
