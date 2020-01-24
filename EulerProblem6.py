8e5ada43dcf5980cee47a68528282b7e1d5659ca
JTS3 <JamesTSmithIII@gmail.com>
2020-01-24 17:06:23 -0500

    Fixed a typo

--- EulerProblem6.py
+++ EulerProblem6.py
@@ -1,4 +1,4 @@
-#Project Euler 5  Sum square difference (http://projecteuler.net/problem=6)
+#Project Euler 6  Sum square difference (http://projecteuler.net/problem=6)
 #Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 n = 100
