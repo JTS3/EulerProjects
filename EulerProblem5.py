074635de645deb36e4f23e00e67d82e6669f7ecd
JTS3 <JamesTSmithIII@gmail.com>
2020-01-24 15:36:59 -0500

    Redid the problem with the least common multiple.

--- EulerProblem5.py
+++ EulerProblem5.py
@@ -2,34 +2,48 @@
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
+#First get the greatest common divisor
+def gcd1(inputvector):
+    if len(inputvector)!=2:
+        return("Error need list with 2 elements")
+    if len(inputvector)==2:
+        if any(inputvector)==0:
+            return(inputvector)
+        if any(inputvector)<0:
+            return("Error Negative INputs")
+        if all(inputvector)>0:
+            inputvector = sorted(inputvector,reverse=True)
+            bigger = inputvector[0]
+            smaller = inputvector[1]
+            inputvector[0] = smaller
+            inputvector[1] = bigger % smaller
+            return(inputvector)
 
-def SequentialMultipleCheck(target, greatest_integer):
-    smallest_integer = 2 #skip 1, anything mod 1 is = 0
-    n = smallest_integer
-    while n <= greatest_integer:
-        if target % n != 0:
-            return(False) 
-        else:
-            n += 1
-    return(True)
+def gcd(inputvector):
+    if len(inputvector)!=2:
+        return("Error need list with 2 elements")
+    while inputvector[1]!=0:
+        inputvector=gcd1(inputvector)
+    return(max(inputvector))
 
 
+#Get the least common multiple
 
-SequentialMultipleCheck(2520,10)
-
-
-
-#now run through the even numbers (it must be even as target % 2 == 0) after 2520 (which we know is the smallest product for the 1 to 10 range)
-
-target = 2520
-ENDER = 0
-while ENDER == 0:
-    if SequentialMultipleCheck(target,20):
-        print("HERE",target)
-        ENDER = 1
+def lcm(inputvector):
+    if len(inputvector)!=2:
+        return("Error need list with 2 elements")
     else:
-        print("nope",target)
-        target += 2
-        #ENDER = 0
+        return(abs(inputvector[0]*inputvector[1])/gcd(inputvector))
 
-#This will do it, but there is a much smarter way to do this... probably taking advantage of primes
+
+
+#Calculate the smallest multiple
+
+answer = 1
+maxmultiple = 20
+for i in range(1,maxmultiple):
+    whoop = lcm([answer,i])
+    answer = max(answer,whoop)
+
+print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is ", answer)
+
