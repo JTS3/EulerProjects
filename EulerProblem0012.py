364e7f84aae7688874adc54746a86c4be5a4ae9f
JTS3 <JamesTSmithIII@gmail.com>
2020-01-28 11:26:52 -0500

    Fixed the wrong variable in the divisor checker.

--- EulerProblem0012.py
+++ EulerProblem0012.py
@@ -2,18 +2,19 @@
 #What is the value of the first triangle number to have over five hundred divisors?
 
 
-target = 500
+target = 5
 n = 0 
 i = 1
 while n < target:
 #Get triangular number
     triangularnumber = i*(i+1)/2
     #Count its divisors call it n
-    n = sum([i%f==0 for f in range(1,i+1)])
+    n = sum([triangularnumber%f==0 for f in range(1,triangularnumber+1)])
     #if the count of divisors > 500 then stop
+    print(i,triangularnumber,n)
     i += 1
+
 
 print("The first triangular number with", target," divisors is", i, "with", n, "factors")
 
 
-#This will do the job but it is slow. 
