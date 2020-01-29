5e37f7d6f72deebda51d0158c08bb3f2efacd2e4
JTS3 <JamesTSmithIII@gmail.com>
2020-01-29 14:51:27 -0500

    Implemented divisor function approach, much faster.

--- EulerProblem0012.py
+++ EulerProblem0012.py
@@ -2,14 +2,34 @@
 #What is the value of the first triangle number to have over five hundred divisors?
 
 
+def primefactorizer(a):
+    factors = []
+    i=2
+    while i <= a:
+        #print(i,a)
+        if a % i==0:
+            factors.append(i)
+            a = a / i 
+        else:
+            i += 1
+    return(factors)
+
+
+
 target = 500
-n = 0 
+n = 1 
 i = 1
 while n < target:
 #Get triangular number
     triangularnumber = int(i*(i+1)/2)
-    #Count its divisors call it n
-    n = sum([triangularnumber%f==0 for f in range(1,triangularnumber+1)])
+    #factor the triangular number into primes
+    primefactors = primefactorizer(triangularnumber)
+    n = 1
+    for p in set(primefactors):
+        a = primefactors.count(p)
+        n = n*(a+1)
+        #Count its divisors call it n
+        #n = sum([triangularnumber%f==0 for f in range(1,triangularnumber+1)])
     #if the count of divisors > 500 then stop
     print(i,triangularnumber,n)
     i += 1
@@ -17,5 +37,3 @@ while n < target:
 
 print("The first triangular number with", target," divisors is", i, "with", n, "factors")
 
-
-#This will do the job, but it is slow. 
